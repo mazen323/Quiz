271b6a73176de8e369209f7cbccc87f9f0b96999
mazen323 <57352732+mazen323@users.noreply.github.com>
2023-03-30 22:34:18 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load("particles-js", "https://ahmed-menisy.github.io/Quizz/assets/particles.json", function () {
    console.log("callback - particles.js config loaded");
    
